c51e698738fd4783960addfd77586322a7766276
Dann Berg <dann@dann-berg.com>
2015-08-03 18:26:41 -0400

    Add templates.

--- app/views.py
+++ app/views.py
@@ -1,6 +1,25 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return "Hello, World!"
+	user = {'nickname': 'Dann'}
+	posts = [ #fake array of posts
+			{
+					'author': {'nickname': 'John'},
+					'body': 'Beautiful day in New York!'
+			},
+			{
+					'author': {'nickname': 'Susan'},
+					'body': 'The movie Dogville was so cool!'
+			},
+			{
+					'author': {'nickname': 'Avital'},
+					'body': 'I just want to dance.'
+			}
+	]
+	return render_template('index.html',
+													title='Home',
+													user=user,
+													posts=posts)
